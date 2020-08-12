45abfd0822770c1360ac2f8f2b73c0933519d381
John Roberts <john.calvin.roberts@gmail.com>
2020-08-13 00:57:18 +0800

    feat: initial routing setup
    Using wouter for routing.
    see: #1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,28 @@
 import React, { FC, StrictMode } from 'react';
-import { Route } from 'wouter';
-import { Root, Settings } from '../pages';
+import { Route, Switch } from 'wouter';
+import { Root, Settings, NotFound } from '../pages';
 
+/**
+ * App - main entry point of our frontend application
+ *
+ * All context/state wrappers, as well as layout wrappers should go here.
+ *
+ * Regarding routing, we wrap all the routes in <Switch> component, so that we can fall back to the <NotFound/> component for 404s
+ */
 const App: FC = () => {
   return (
     <StrictMode>
-      <Route path="/">
-        <Root />
-      </Route>
-      <Route path="/settings">
-        <Settings />
-      </Route>
+      <Switch>
+        <Route path="/">
+          <Root />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </StrictMode>
   );
 };
