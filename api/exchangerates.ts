0f2d67a641cbd9ddf9e0592809489257bf8462b5
John Roberts <john.calvin.roberts@gmail.com>
2021-02-17 02:02:37 +0800

    fix: use url

--- api/exchangerates.ts
+++ api/exchangerates.ts
@@ -2,9 +2,9 @@ import fetch from 'node-fetch';
 import { NowRequest, NowResponse } from '@vercel/node';
 
 export default async (request: NowRequest, response: NowResponse) => {
-  const { query } = request;
+  const { query, url } = request;
   const toUpstream = new URLSearchParams(query as Record<string, any>);
-  const res = await fetch(`https://api.exchangeratesapi.io?${toUpstream.toString()}`);
+  const res = await fetch(`https://api.exchangeratesapi.io${url}?${toUpstream.toString()}`);
   const json = await res.json();
   response.status(200).json(json);
 };
