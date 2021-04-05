472d587dfe811fde3c7308bb4c424607a916c8a3
John Roberts <john.calvin.roberts@gmail.com>
2021-04-05 21:40:32 +0800

    fix: missing access token

--- api/exchangerates/index.ts
+++ api/exchangerates/index.ts
@@ -3,8 +3,11 @@ import { NowRequest, NowResponse } from '@vercel/node';
 
 export default async (request: NowRequest, response: NowResponse) => {
   const { query } = request;
+  const ACCESS_TOKEN = process.env.EXCHANGE_RATES_API_ACCESS_TOKEN;
   const toUpstream = new URLSearchParams(query as Record<string, any>);
-  const res = await fetch(`https://api.exchangeratesapi.io?${toUpstream.toString()}`);
+  const res = await fetch(
+    `https://api.exchangeratesapi.io?${toUpstream.toString()}&access_key=${ACCESS_TOKEN}`,
+  );
   const json = await res.json();
   response.status(200).json(json);
 };
--- api/exchangerates/latest.ts
+++ api/exchangerates/latest.ts
@@ -4,7 +4,10 @@ import { NowRequest, NowResponse } from '@vercel/node';
 export default async (request: NowRequest, response: NowResponse) => {
   const { query } = request;
   const toUpstream = new URLSearchParams(query as Record<string, any>);
-  const res = await fetch(`https://api.exchangeratesapi.io/latest?${toUpstream.toString()}`);
+  const ACCESS_TOKEN = process.env.EXCHANGE_RATES_API_ACCESS_TOKEN;
+  const res = await fetch(
+    `https://api.exchangeratesapi.io/latest?${toUpstream.toString()}&access_key=${ACCESS_TOKEN}`,
+  );
   const json = await res.json();
   response.status(200).json(json);
 };
